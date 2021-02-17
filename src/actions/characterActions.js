a4865f3fbd935715751ead28b9f3f54617c07368
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-02-17 18:44:13 +0600

    Add list action

---
+++ src/actions/characterActions.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { getApiHash } from 'marvel-api-hash-generator';
+
+const GetCharacterList = page => async dispatch => {
+  try {
+    dispatch({
+      type: 'CHARACTER_LIST_LOADING',
+    });
+    const timeStamp = 1;
+    const privateKey = 'c9384891527cc3bd72cab81dce560d18174a46bb';
+    const publicKey = '14946262ad54655fac24826eeceeb49b';
+    const hashValue = getApiHash(timeStamp, privateKey, publicKey);
+
+    const perPage = 15;
+    const offset = page * perPage - perPage;
+
+    const requestConstantCharacters = `https://gateway.marvel.com/v1/public/series/9085/characters?limit=15&offset=${offset}`;
+
+    const url = `${requestConstantCharacters}&ts=${timeStamp}&apikey=${publicKey}&hash=${hashValue}`;
+    const result = await axios.get(url);
+    dispatch({
+      type: 'CHARACTER_LIST_SUCCESS',
+      payload: result.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: 'CHARACTER_LIST_FAIL',
+    });
+  }
+};
+
+export default GetCharacterList;
