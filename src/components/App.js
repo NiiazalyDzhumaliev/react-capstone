08ef0af59e90ab104f96a55eedc9e64004aa9ee9
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-02-17 18:20:27 +0600

    Add navlink

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 import axios from 'axios';
 import { getApiHash } from 'marvel-api-hash-generator';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import {
+  Switch, Route, Redirect, NavLink,
+} from 'react-router-dom';
 import characterList from '../containers/characterList';
 import character from '../containers/character';
 import '../App.css';
@@ -26,6 +28,9 @@ const App = () => {
 
   return (
     <div className="App">
+      <nav>
+        <NavLink to="/">Search</NavLink>
+      </nav>
       <Switch>
         <Route path="/" exact component={characterList} />
         <Route path="/character/:character" component={character} />
