376ff6eb9f5183064eef20c5eba09e5016d375fd
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-02-17 20:34:14 +0600

    Add character reducer

--- src/reducers/character.js
+++ src/reducers/character.js
@@ -1,7 +1,36 @@
-import React from 'react';
+const defaultState = {
+  loading: false,
+  data: {},
+  error: '',
+};
 
-function character() {
-  return <div />;
-}
+const characterReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'POKEMON_MULTIPLE_LOADING':
+      return {
+        ...state,
+        loading: true,
+        error: '',
+      };
+    case 'POKEMON_MULTIPLE_FAIL':
+      return {
+        ...state,
+        loading: false,
+        error: 'unable to find a character',
+      };
+    case 'POKEMON_MULTIPLE_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        error: '',
+        data: {
+          ...state.data,
+          [action.characterName]: action.payload,
+        },
+      };
+    default:
+      return state;
+  }
+};
 
-export default character;
+export default characterReducer;
