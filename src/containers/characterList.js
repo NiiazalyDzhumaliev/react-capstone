c8a266630dacca66d762fa7bb138fb6167522a80
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-03-23 14:01:19 +0600

    Delete no image filtering and add char name size

--- src/containers/characterList.js
+++ src/containers/characterList.js
@@ -18,45 +18,34 @@ const CharacterList = () => {
     fetchData();
   }, []);
 
-  const showData = () => {
-    const noImageURL = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg';
-    return (
-      <InfiniteScroll
-        dataLength={characterList.data.length}
-        next={() => fetchData(
-          characterList.data.length / 15 === 1
-            ? 2
-            : characterList.data.length / 15,
-        )}
-        hasMore
-        loader={<h4>Loading...</h4>}
-        className={style.char_container}
-      >
-        {characterList.data.map(character => {
-          if (
-            noImageURL
-            !== `${character.thumbnail.path}.${character.thumbnail.extension}`
-          ) {
-            return (
-              <div key={uuidv4()}>
-                <p>{character.name}</p>
+  const showData = () => (
+    <InfiniteScroll
+      dataLength={characterList.data.length}
+      next={() => fetchData(
+        characterList.data.length / 15 === 1
+          ? 2
+          : characterList.data.length / 15,
+      )}
+      hasMore
+      loader={<h4>Loading...</h4>}
+      className={style.char_container}
+    >
+      {characterList.data.map(character => (
+        <div key={uuidv4()}>
+          <p className={style.char_name}>{character.name}</p>
 
-                <Link to={`/character/${character.name}`}>
-                  {' '}
-                  <img
-                    className={style.char_image}
-                    alt="marvel"
-                    src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-                  />
-                </Link>
-              </div>
-            );
-          }
-          return null;
-        })}
-      </InfiniteScroll>
-    );
-  };
+          <Link to={`/character/${character.name}`}>
+            {' '}
+            <img
+              className={style.char_image}
+              alt="marvel"
+              src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+            />
+          </Link>
+        </div>
+      ))}
+    </InfiniteScroll>
+  );
   return (
     <div>
       <div>
